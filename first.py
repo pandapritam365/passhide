95d09fcb67c9d5db2cf0eaab255d3342b8aaf6b7
pandapritam365 <55808706+pandapritam365@users.noreply.github.com>
2020-11-29 21:40:33 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,4 @@
+username=input("enter the user name\t")
+password=input("Enter the password\t")
+hidden_password=len(password)*"*"
+print(f"yeah! your username is {username} \n your secret password is\t{hidden_password}")
